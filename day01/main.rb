2995b2ad12e95d686f0afc3731c12d1f0daa7ccf
Sam Vendittelli <sam.vendittelli@hotmail.com>
2025-12-05 09:20:26 +0000

    feat: day 1 part 2 (the dumb way)

--- day01/main.rb
+++ day01/main.rb
@@ -4,15 +4,28 @@ lines = File.readlines(File.join(__dir__, file_name))
 
 # The dial starts at 50 initially
 value = 50
-count = 0
+count1 = 0
+count2 = 0
 
 lines.each do |line|
-  line[0] == "L" ? value -= line[1..].to_i : value += line[1..].to_i
+  dir = line[0] == "L" ? -1 : 1
+  dist = line[1..].to_i
+
+  old_value = value
+  value += dir * dist
   value = value % 100
+
   # puts value
   if value == 0
-    count += 1
+    count1 += 1
+  end
+
+  dist.times do |i|
+    if (old_value + (dir * i)) % 100 == 0
+      count2 += 1
+    end
   end
 end
 
-puts count
+puts "Part 1: " + count1.to_s
+puts "Part 2: " + count2.to_s
