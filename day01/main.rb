5c2bec7d8594337e39ffb663ab9a122e55ceaf10
Sam Vendittelli <sam.vendittelli@hotmail.com>
2025-12-05 17:32:31 +0000

    feat: format all files with rubocop

--- day01/main.rb
+++ day01/main.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 # file_name = "example.txt"
-file_name = "input.txt"
+file_name = 'input.txt'
 lines = File.readlines(File.join(__dir__, file_name))
 
 # The dial starts at 50 initially
@@ -8,24 +10,20 @@ count1 = 0
 count2 = 0
 
 lines.each do |line|
-  dir = line[0] == "L" ? -1 : 1
+  dir = line[0] == 'L' ? -1 : 1
   dist = line[1..].to_i
 
   old_value = value
   value += dir * dist
-  value = value % 100
+  value %= 100
 
   # puts value
-  if value == 0
-    count1 += 1
-  end
+  count1 += 1 if value.zero?
 
   dist.times do |i|
-    if (old_value + (dir * i)) % 100 == 0
-      count2 += 1
-    end
+    count2 += 1 if ((old_value + (dir * i)) % 100).zero?
   end
 end
 
-puts "Part 1: " + count1.to_s
-puts "Part 2: " + count2.to_s
+puts "Part 1: #{count1}"
+puts "Part 2: #{count2}"
